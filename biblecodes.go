377f30a5cdf2766b48780683c7cf19182af647a3
believer3773 <believer3773@gmail.com>
2021-01-10 00:22:06 -0500

    small update

--- biblecodes.go
+++ biblecodes.go
@@ -1348,7 +1348,7 @@ func main() {
 
 	indextmpl := template.Must(template.ParseFiles("index.html"))
 	//data.PageTitle = "A Survey of Biblical Numerics and Mathematical Monotheism (Bible Codes)"
-	data.PageTitle = "scripturecodes.com provides free access to Bible codes."
+	data.PageTitle = "scripturecodes.com provides a summarized view of some recent Bible codes."
 	sendEarlyHtml_fd(indexfd) //Write any early html like <head> and <style>
 	indextmpl.Execute(indexfd, data)
 
